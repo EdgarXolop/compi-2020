832fa5d2d02b38a8a27e1e1e8c7fc446e6b7aeea
Edgar Xolop <edgarxoix@gmail.com>
2020-05-31 23:37:43 -0600

    fase 2

---
+++ webb-app/src/components/Errors.vue
@@ -0,0 +1,16 @@
+<template>
+  <div >
+    <div class="alert alert-danger" role="alert" v-for="(error,index) in errors" v-bind:key="index">
+      {{error}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Errors',
+  props: {
+    errors: Array
+  }
+}
+</script>
